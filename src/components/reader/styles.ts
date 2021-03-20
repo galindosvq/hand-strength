66d7ffc49ed6b58a9c177161a1729cc7cd9a2673
Antonio Galindo <galindosvq@gmail.com>
2021-03-21 00:31:33 +0100

    feat(Reader): create reader module

---
+++ src/components/reader/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  padding: 5rem;
+  border: 1px solid;
+`;
+
+export const Title = styled.h1`
+  color: grey;
+`;
