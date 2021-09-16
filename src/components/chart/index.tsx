eee436dc5d291c3d62a784ee34e4eec379ec2101
Antonio Galindo <galindosvq@gmail.com>
2021-09-16 16:31:16 +0200

    chore: add axis Y test

--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -1,5 +1,5 @@
 import { ParseResult } from "papaparse";
-import React, { memo, useCallback } from "react";
+import React, { memo, useCallback, useState } from "react";
 import {
   LineChart,
   Line,
@@ -22,30 +22,47 @@ const Chart = ({ data }: Props) => {
     return { time_ms: value[0], weight: value[1] };
   });
 
+  const [ticks, setTicks] = useState([
+    0,
+    5,
+    10,
+    15,
+    20,
+    25,
+    30,
+    35,
+    40,
+    45,
+    50,
+  ]);
+
+  const addTicks = useCallback(() => {
+    setTicks((current) => [...current, ticks[ticks.length - 1] + 25]);
+  }, [ticks]);
+
   return (
-    <ResponsiveContainer height={400} width="100%">
-      <AreaChart data={formatData.slice(1, -1)}>
-        <CartesianGrid strokeDasharray="2 2" />
-        <XAxis
-          dataKey="time_ms"
-          ticks={[60000]}
-          label="Time (ms)"
-          tickSize={10}
-        />
-        <YAxis
-          dataKey="weight"
-          ticks={[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]}
-          tickSize={15}
-        />
-        <Area
-          dataKey="weight"
-          stroke="#662a2a"
-          fill="#242d53"
-          name="Strength (Kg)"
-        />
-        <Tooltip />
-      </AreaChart>
-    </ResponsiveContainer>
+    <>
+      <button onClick={addTicks}>ADD TICK</button>
+      <ResponsiveContainer height={400} width="100%">
+        <AreaChart data={formatData.slice(1, -1)}>
+          <CartesianGrid strokeDasharray="2 2" />
+          <XAxis
+            dataKey="time_ms"
+            ticks={[60000]}
+            label="Time (ms)"
+            tickSize={10}
+          />
+          <YAxis dataKey="weight" ticks={ticks} tickSize={15} />
+          <Area
+            dataKey="weight"
+            stroke="#662a2a"
+            fill="#242d53"
+            name="Strength (Kg)"
+          />
+          <Tooltip />
+        </AreaChart>
+      </ResponsiveContainer>
+    </>
   );
 };
 
