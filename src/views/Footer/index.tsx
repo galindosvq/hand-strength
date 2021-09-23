d328e548a170a5a2105e4bde2f4345eca1e2ccaa
Antonio Galindo <galindosvq@gmail.com>
2021-09-23 12:15:21 +0200

    feat: create footer

---
+++ src/views/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Foot } from "./style";
+
+const Footer = () => (
+  <Foot>
+    lovingly made for the community by
+    <a href="https://galindosvq.github.io/">Antonio Galindo</a>
+  </Foot>
+);
+
+export default Footer;
---
+++ src/views/Footer/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Foot = styled.footer`
+  padding: 15px 0;
+  font-size: 0.8rem;
+  text-align: center;
+
+  & a {
+    text-decoration: none;
+    color: gray;
+  }
+`;
