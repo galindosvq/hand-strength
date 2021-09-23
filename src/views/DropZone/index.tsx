c4d6a043636ffb130f3fd21f9c8ecef462df5dc6
Antonio Galindo <galindosvq@gmail.com>
2021-09-23 12:38:06 +0200

    refactor: move to logic module

--- src/views/DropZone/index.tsx
+++ src/views/DropZone/index.tsx
@@ -3,47 +3,21 @@ import { CSVReader } from "react-papaparse";
 import Chart from "views/Chart";
 import { useDropZone } from "./logic";
 import { DropSpan, ReaderContainer } from "./style";
+import { data } from "utils/formatter";
+import { ParseResult } from "papaparse";
 
-const DropZone = () => {
-  const {
-    file,
-    handleOnDrop,
-    handleOnError,
-    handleOnRemoveFile,
-  } = useDropZone();
+type Props = {
+  handleOnDrop: (file: any) => void;
+  handleOnError: () => void;
+  handleOnRemoveFile: () => void;
+};
 
-  const styles = {
-    dropArea: {
-      borderColor: "#3b027c",
-      borderRadius: 8,
-      borderWidth: "3px",
-      width: "50%",
-      margin: "50px 0 0",
-    },
-    dropFile: {
-      width: 70,
-      height: 120,
-      background: "#886bcca4",
-      margin: "10px 0",
-    },
-    fileSizeInfo: {
-      display: "none",
-    },
-    fileNameInfo: {
-      color: "#ffffff",
-      backgroundColor: "#391864",
-      borderRadius: 3,
-      fontSize: 16,
-      lineHeight: 1,
-      padding: "5px 8px",
-    },
-    removeButton: {
-      color: "#3b027c",
-    },
-    progressBar: {
-      backgroundColor: "#7e23a8",
-    },
-  };
+const DropZone = ({
+  handleOnDrop,
+  handleOnError,
+  handleOnRemoveFile,
+}: Props) => {
+  const { readerStyles } = useDropZone();
 
   return (
     <>
@@ -53,12 +27,11 @@ const DropZone = () => {
           onError={handleOnError}
           addRemoveButton
           onRemoveFile={handleOnRemoveFile}
-          style={styles}
+          style={readerStyles}
         >
           <DropSpan>Drop a CSV file here or click to upload</DropSpan>
         </CSVReader>
       </ReaderContainer>
-      {file && <Chart data={file} />}
     </>
   );
 };
--- src/views/DropZone/logic.ts
+++ src/views/DropZone/logic.ts
@@ -16,5 +16,44 @@ export const useDropZone = () => {
     setFile(undefined);
   }, []);
 
-  return { file, handleOnDrop, handleOnError, handleOnRemoveFile };
+  const readerStyles = {
+    dropArea: {
+      borderColor: "#3b027c",
+      borderRadius: 8,
+      borderWidth: "3px",
+      width: "50%",
+      margin: "50px 0 0",
+    },
+    dropFile: {
+      width: 70,
+      height: 120,
+      background: "#886bcca4",
+      margin: "10px 0",
+    },
+    fileSizeInfo: {
+      display: "none",
+    },
+    fileNameInfo: {
+      color: "#ffffff",
+      backgroundColor: "#391864",
+      borderRadius: 3,
+      fontSize: 16,
+      lineHeight: 1,
+      padding: "5px 8px",
+    },
+    removeButton: {
+      color: "#3b027c",
+    },
+    progressBar: {
+      backgroundColor: "#7e23a8",
+    },
+  };
+
+  return {
+    file,
+    handleOnDrop,
+    handleOnError,
+    handleOnRemoveFile,
+    readerStyles,
+  };
 };
