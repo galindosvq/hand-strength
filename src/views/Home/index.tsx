6d2b8bb2be9067c1fd250ae5ab77ee75bc068f1d
Antonio Galindo <galindosvq@gmail.com>
2021-09-23 12:39:05 +0200

    refactor: handle state from Home view

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,14 +1,26 @@
 import Chart from "views/Chart";
 import DropZone from "views/DropZone";
+import { useDropZone } from "views/DropZone/logic";
 import Footer from "views/Footer";
 import { Container, Main } from "./style";
 
 const Home = () => {
+  const {
+    file,
+    handleOnDrop,
+    handleOnError,
+    handleOnRemoveFile,
+  } = useDropZone();
+
   return (
     <Container>
       <Main>
-        <DropZone />
-        <Chart data={[]} />
+        <DropZone
+          handleOnDrop={handleOnDrop}
+          handleOnError={handleOnError}
+          handleOnRemoveFile={handleOnRemoveFile}
+        />
+        <Chart data={file} />
       </Main>
       <Footer />
     </Container>
